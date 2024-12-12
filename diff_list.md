43c242f0dc75203d373820e4101ab06ea385cadb
Jens <134612744+JensOgorek@users.noreply.github.com>
2024-12-12 17:36:55 +0100

    General enhancements (#4)
    * add gitignore
    
    * add diff list
    
    * script for 1-parse.h
    
    * owl example compile script
    
    * chmod+x
    
    * fix directory
    
    * no renaming needed
    
    * removed
    
    * change to default name
    
    * updated diff list
    
    * update: gen 1parse text

---
+++ diff_list.md
@@ -0,0 +1,7 @@
+Changes in this fork
+
+ .gitignore:
+    - Add compiled owl executable to the ignore list
+
+gen_1-parse.sh:
+    - Add a script for generating 1-parse.h more quickly for debugging
---
+++ gen_1-parse.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "Generating parser..."
+# Check if any file in src/ is newer than 1-parse.h or if 1-parse.h does not exist
+if [[ ! -f src/1-parse.h ]] || find src/ -type f ! -name "1-parse.h" -newer src/1-parse.h | grep -q .
+then
+    echo "Source files have changed or parser file is missing. Regenerating parser..."
+    make
+    ./owl -c grammar.owl -o src/1-parse.h
+    if [[ $? -ne 0 ]]
+    then
+        rm -f src/1-parse.h
+        exit 1
+    fi
+
+else
+    echo "Nothing to do."
+fi
+
+echo "Done."
